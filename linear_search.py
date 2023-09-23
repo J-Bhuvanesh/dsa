728ef49f44401105a9388c1aa2649d3832472ee9
Bhuvanesh <bhuvanesh@divum.in>
2023-09-23 13:10:00 +0530

    linear search completed

--- linear_search.py
+++ linear_search.py
@@ -1,10 +1,11 @@
+def linear_search(element, element_to_search):
+    for ele in element:
+        if ele == element_to_search:
+            return "Element found in a list"
+    return "Element not found in the list"
 
 
-
-
-
-def linear_search():
-    pass
-
-
-linear_search()
+element = [2, 3, 467, 89, 12, 34, 5, 6, 7]
+element_to_search = 1
+result = linear_search(element, element_to_search)
+print(result)
